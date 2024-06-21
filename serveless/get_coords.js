0ed041c9364f9059a5929ff0ac89b56489ec5e5f
ChristoBongende <100066566+bongende@users.noreply.github.com>
2024-06-21 17:00:36 +0200

    -rf

--- serveless/get_coords.js
+++ serveless/get_coords.js
@@ -1,5 +1,29 @@
 const fetch = require("node-fetch");
-const WEATHER_API_KEY = process.env;
+
+const { WEATHER_API_KEY } = process.env;
+
+exports.handler = async (event, context) => {
+  const params = JSON.parse(event.body);
+  const { text, units } = params;
+  const regex = /^\d+$/g;
+  const flag = regex.test(text) ? "zip" : "q";
+  const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${text}&units=${units}&appid=${WEATHER_API_KEY}`;
+  const encodedUrl = encodeURI(url);
+  try {
+    const dataStream = await fetch(encodedUrl);
+    const jsonData = await dataStream.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(jsonData),
+    };
+  } catch (err) {
+    return { statusCode: 422, body: err.stack };
+  }
+};
+
+/*
+const fetch = require("node-fetch");
+const { WEATHER_API_KEY } = process.env;
 
 exports.handler = async (event, context) => {
   const params = JSON.parse(event.body);
@@ -8,7 +32,7 @@ exports.handler = async (event, context) => {
   const regex = /^\d+$/g;
   const flag = regex.test(text) ? "zip" : "q";
 
-  const url = `http://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}id=524901&appid=${WEATHER_API_KEY}`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?${flag}=${text}&units=${units}id=524901&appid=${WEATHER_API_KEY}`;
   const encodedUrl = encodeURI(url);
   try {
     const dataStream = await fetch(encodedUrl);
@@ -24,3 +48,5 @@ exports.handler = async (event, context) => {
     };
   }
 };
+
+*/
